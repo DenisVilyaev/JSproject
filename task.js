5bea928fee9aec4bab3fcfb946adceb4f2c3d176
DenisVilyaev <vilaevdenis007@gmail.com>
2026-04-18 19:37:05 +0300

    added new logic

--- task.js
+++ task.js
@@ -1,9 +1,27 @@
-console.log("hello js")
+console.log("hello js");
 
-let numb1 = 1
-let numb2 = 10
+let numb1 = 1;
+let numb2 = 10;
 
-a = 3
-b = 4
-console.log(a + b)
-console.log(a - b)
+a = 3;
+b = 4;
+console.log(a + b);
+console.log(a - b);
+
+// Урок: операторы и условия
+
+title = "";
+if ((title = "")) {
+  console.log("Название задачи не указано");
+} else {
+  console.log("Задача:");
+}
+
+tasks = 0;
+if ((tasks = 0)) {
+  console.log("Список пуст");
+} else if (tasks > 0 && tasks <= 3) {
+  console.log("Немного задач");
+} else {
+  console.log("Много задач");
+}
